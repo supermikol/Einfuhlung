3f51ac21ded6a1df8034648b6c1cef74ae3f59a2
supermikol <mdu123@gmail.com>
2016-01-28 00:05:42 -0800

    v2

--- app/controllers/auth.rb
+++ app/controllers/auth.rb
@@ -1,11 +1,36 @@
 get '/' do
-  erb :welcome
+  if request.xhr?
+    erb :welcome, layout: false
+  else
+    erb :welcome
+  end
 end
 
 get '/login' do
-  erb :login
+  if request.xhr?
+    erb :login, layout: false
+  else
+    erb :login
+  end
 end
 
 get '/signup' do
   erb :signup
 end
+
+get '/about' do
+  if request.xhr?
+    erb :about, layout: false
+  else
+    erb :about
+  end
+end
+
+get '/contact' do
+  if request.xhr?
+    erb :contact, layout: false
+  else
+    erb :contact
+  end
+end
+
--- public/js/application.js
+++ public/js/application.js
@@ -4,4 +4,27 @@ $(document).ready(function() {
   // when we try to bind to them
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  $('#navbar').on('click', 'a', function(event){
+    event.preventDefault();
+    link = $(this).attr('href');
+    $.ajax({
+      method: 'GET',
+      url: link
+    }).done(function(response){
+      $('.starter-template').html(response);
+    });
+
+  });
+
+  $('#welcomeLogin').on('click','a',function(e){
+    e.preventDefault();
+    link = $(this).attr('href');
+    $.ajax({
+      method: 'GET',
+      url: link
+    }).done(function(response){
+      $('.starter-template').html(response);
+    });
+  })
+
 });
