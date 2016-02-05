bfa54b93086b59c64f69c9bfc016c602a5c0d55d
Michael Du <email": null>
2016-02-04 17:01:36 -0800

    Refactored code for messaging

--- app/models/message.rb
+++ app/models/message.rb
@@ -12,5 +12,38 @@ class Message < ActiveRecord::Base
 
     has_one :recipient, through: :parent_message, source: :sender
 
+    #returns array of thread(s), will include multiple threads if it is first post
+
+    def last_child_id
+      if self.children_messages.length > 0
+        return self.children_messages[0].last_child_id
+      else
+        return self.id
+      end
+    end
+
+    def return_threads
+    threads = Array.new
+    if self.parent_message
+      thread = [self.parent_message, self]
+      temp_message = self
+      until temp_message.children_messages.length == 0
+        temp_message = temp_message.children_messages[0]
+        thread <<  temp_message
+      end
+      threads << thread
+    else
+      self.children_messages.each do |message|
+        thread = [message.parent_message, message]
+        temp_message = message
+        until temp_message.children_messages.length == 0
+          temp_message = temp_message.children_messages[0]
+          thread <<  temp_message
+        end
+        threads << thread
+      end
+    end
+    return threads
+  end
 
 end
